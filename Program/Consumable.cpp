a405f9f50882d44a3981574b4e7c09b084e9ee4a
changgyu359 <gchl8548@gmail.com>
2025-09-15 19:45:33 +0900

    (2025.9.15) 얕은 복사와 깊은 복사

---
+++ Program/Consumable.cpp
@@ -0,0 +1,11 @@
+#include "Consumable.h"
+
+Consumable::Consumable()
+{
+	cout << "Created Consumalbe" << endl;
+}
+
+Consumable::~Consumable()
+{
+	cout << "Destroy Consumalbe" << endl;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,95 +1,18 @@
 ﻿#include <iostream>
+#include "Consumable.h" // 사용자 정의 헤더는 쌍따옴표로 가져옴
 
 using namespace std;
 
-class Unit
-{
-private:
-	static int count;
-public:
-	
-	Unit() {
-		cout << "Created Unit"<<endl;
-	}
-	
-	~Unit() {
-		count++;
-		cout << "Count:"<< count << endl;
-	}
-};
-
-class Card
-{
-private:
-	char grade;
-	const char* name;
-
-public:
-
-	Card() = default;
-
-	Card(Card& clone) // 레퍼런스가 안들어가면 무한루프에 빠짐
-	{
-
-	}
-
-	void initialize(char grade, const char* name)
-	{
-		// this : 자기 자신을 가리키는 포인터 변수.
-		this->grade = grade;
-		this->name = name;
-	}
-
-	
-
-};
-
-
-
-
-int Unit::count = 0;
 
 int main()
 {
-#pragma region 생성자
-	// 클래스의 인스턴스가 생성되는 시점에 자동으로 호출되는 특수한 멤버 함수.
-	//Unit unit;
-
-	// 생성자의 경우 객체가 생성될 때 단 한 번만 호출되며, 생성자는 반환형이
-	// 존재하지 않기 때문에 생성자가 호출되기 전에는 객체에 대한 메모리가 
-	// 할당되지 않음.
-
-	// 사용법: 클래스의 이름과 똑같은 이름으로 (){}을 클래스 내부에 써주면 됨
-#pragma endregion
-
-#pragma region 소멸자
-	// 객체가 소멸될 때 자동으로 실행되는 클래스의 멤버 함수.
-
-	// 소멸자는 객체가 메모리에서 해제될 때 단 한 번만 호출되며,
-	// 소멸자에게는 매개 변수를 생성하여 사용할 수 없습니다.
-
-	// 사용법: 생성자와 비슷한데, 그 앞에 ~를 붙이면 됨.
-
-	//Unit* creature=new Unit;
-	//delete creature;
-	//creature = new Unit;
-	//delete creature;
-
-
-	// 동적할당 기억하기: 자료형* 이름= new 자료형;
+#pragma region 상속
+	// 상위 클래스의 속성을 하위 클래스가 사용할 수 있도록 설정해주는 기능.
 	
 	
-#pragma endregion
 
-#pragma region 복사 생성자
-	// 같은 객체를 복사하여 생성시킬 때 호출되는 생성자.
+	Consumable consumable; // 이거 사용하려면 헤더 파일 가져와야함->#include필요
 
-	Card card;
-
-	card.initialize('A', "Diamond");
-
-	// 복사 생성자 사용법. 클래스 이름 사용이름(복사대상 이름) or 클래스이름 사용이름 =복사대상;
-	Card clone(card);
 #pragma endregion
 
 
