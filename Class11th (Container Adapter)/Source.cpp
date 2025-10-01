1ad4f9b895a2e24b7803f94bc86c340355ceb03b
changgyu359 <gchl8548@gmail.com>
2025-10-01 19:47:05 +0900

    (2025.10.1)연관 컨테이너

---
+++ Class11th (Container Adapter)/Source.cpp
@@ -0,0 +1,82 @@
+﻿#include "../Program/stdafx.h"
+#include<stack>
+#include<queue>
+
+
+
+
+int main()
+{
+#pragma region 컨테이너 어댑터
+	// 기존 컨테이너의 인터페이스를 제한하여 만든 기능이 제한되거나 
+	// 변경된 컨테이너
+
+#pragma region stack container
+
+	//std::stack<int> stack;
+
+	//stack.push(10);
+	//stack.push(20);
+	//stack.push(30);
+	//stack.push(40);
+	//stack.push(50);
+
+	//while (stack.empty() == false)
+	//{
+	//	cout << stack.top() << endl;
+
+	//	stack.pop();
+	//}
+	//
+
+
+
+#pragma endregion
+
+#pragma region queue container
+// F(irst) I(n) F(irst) O(ut)
+// 가장 먼저 들어온 데이터가 가장 먼저 나간다
+
+//std::queue<int> queue;
+
+//queue.push(10);
+//queue.push(20);
+//queue.push(30);
+//queue.push(40);
+//queue.push(50);
+
+//const int& size = queue.size();
+
+//for (int i = 0; i < size;i++)
+//{
+//	cout << queue.front() << endl;
+//	queue.pop();
+//}
+
+#pragma endregion
+
+#pragma region priority queue container
+
+	std::priority_queue<int> priority_queue;
+
+	priority_queue.push(40);
+	priority_queue.push(20);
+	priority_queue.push(50);
+	priority_queue.push(100);
+	priority_queue.push(1);
+
+	int size = priority_queue.size();
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << priority_queue.top() << endl;
+		priority_queue.pop();
+	}
+#pragma endregion
+
+#pragma endregion
+
+
+	return 0;
+}
+
--- Program/Source.cpp
+++ Program/Source.cpp
@@ -1,56 +1,36 @@
 ﻿#include "stdafx.h"
-#include<stack>
-#include<queue>
-
+#include <unordered_set>
 
 
 
 int main()
 {
-#pragma region 컨테이너 어댑터
-	// 기존 컨테이너의 인터페이스를 제한하여 만든 기능이 제한되거나 
-	// 변경된 컨테이너
+#pragma region 연관 컨테이너
+	// 특정한 방식으로 데이터를 저장하고 검색하는 컨테이너.
 
-#pragma region stack container
+	std::unordered_set<const char*> unordered_set;
 
-	//std::stack<int> stack;
+	unordered_set.reserve(16);
 
-	//stack.push(10);
-	//stack.push(20);
-	//stack.push(30);
-	//stack.push(40);
-	//stack.push(50);
-
-	//while (stack.empty() == false)
-	//{
-	//	cout << stack.top() << endl;
-
-	//	stack.pop();
-	//}
-	//
+	unordered_set.max_load_factor(2.0);
+	unordered_set.insert("양손검");
+	unordered_set.insert("단검");
+	unordered_set.insert("둔기");
+	unordered_set.insert("방패");
+	unordered_set.insert("지팡이");
+	unordered_set.insert("활");
+	unordered_set.insert("석궁");
+	unordered_set.insert("총");
+	unordered_set.insert("쌍검");
+	unordered_set.insert("쌍검");
+	unordered_set.insert("쌍검");
+	unordered_set.insert("쌍검");
 	
-#pragma region queue container
-	// F(irst) I(n) F(irst) O(ut)
-	// 가장 먼저 들어온 데이터가 가장 먼저 나간다
+	cout << "Load Factor:" << unordered_set.load_factor() << endl;
 
-	std::queue<int> queue;
-
-	queue.push(10);
-	queue.push(20);
-	queue.push(30);
-	queue.push(40);
-	queue.push(50);
-
-	for (int i = 0; i < queue.size();)
-	{
-		cout << queue.front() << endl;
-		queue.pop();
-	}
-
-#pragma endregion
-
-#pragma endregion
-
+	// 0.5 이하(매우 낮음)
+	// 0.5~1.0(고르게 분포)
+	cout << "Bucket Count: "<< unordered_set.bucket_count() << endl;
 
 #pragma endregion
 
