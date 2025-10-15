0299c9fa9006f5f3e9292e0373840ee7cb6192f8
changgyu359 <gchl8548@gmail.com>
2025-10-15 18:05:28 +0900

    d

--- Class7th (Encapsulation)/Source.cpp
+++ Class7th (Encapsulation)/Source.cpp
@@ -1,20 +1,27 @@
-﻿// Class7th (Encapsulation).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include "../Program/stdafx.h"
+#include "../Program/Vector2.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+#pragma region 캡슐화
+    // 객체의 속성과 기능을 하나의 단위 합친 다음 외부에서
+    // 필요한 정보만 접근할 수 있도록 제한하는 기능입니다.
+
+    Vector2 point1D(1, 0);
+    Vector2 point2D(0, 1);
+
+    Vector2 direction = point1D + point2D;
+
+    Vector2 position = direction++;
+
+    cout << "position 객체의 X 값 : " << position.X() << endl;
+    cout << "position 객체의 Y 값 : " << position.Y() << endl;
+
+    cout << "diretion 객체의 X 값 : " << direction.X() << endl;
+    cout << "diretion 객체의 Y 값 : " << direction.Y() << endl;
+
+#pragma endregion
+
+
+    return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
